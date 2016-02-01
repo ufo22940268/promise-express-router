c7a5c9fea319c6815c8b2919be97c16963563271
Frank Cheng <ufo22940268@gmail.com>
2016-02-01 16:34:07 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by cc on 2/1/16.
+ */
+"use strict";
+module.exports = require('./lib/PromiseRouter');
---
+++ test/router-test.js
@@ -0,0 +1,49 @@
+/**
+ * Created by cc on 2/1/16.
+ */
+"use strict";
+
+const PromiseRouter = require('../');
+const express = require('express');
+const request = require('supertest');
+const expect = require("chai").expect;
+
+describe('PromiseRouter', function () {
+  let app;
+  let promiseRouter;
+
+  beforeEach(function () {
+    app = express();
+    promiseRouter = new PromiseRouter();
+    app.use(promiseRouter.create());
+  });
+
+  describe('Get', function () {
+    it("should get promise result 1", function (done) {
+      promiseRouter.getAsync('/test', (req, locals) => {
+        return Promise.resolve({a: 1});
+      });
+
+      request(app)
+        .get('/test')
+        .expect({result: {a: 1}, ok: true})
+        .end(done);
+    });
+
+    it("should response with error stacktrace", function (done) {
+      promiseRouter.getAsync('/error', (req, locals) => {
+        return Promise.reject(new Error());
+      });
+
+      request(app)
+        .get('/error')
+        .expect(200)
+        .end(function (error, res) {
+          expect(res.body).to.have.property('msg');
+          expect(res.body).to.have.property('ok', false);
+          expect(res.body).to.have.property('code');
+          done();
+        });
+    })
+  });
+});
