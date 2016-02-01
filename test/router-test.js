d83fca861065141411e87fe1577063366da2a935
Frank Cheng <ufo22940268@gmail.com>
2016-02-01 19:33:15 +0800

    Support post method.

--- test/router-test.js
+++ test/router-test.js
@@ -46,4 +46,33 @@ describe('PromiseRouter', function () {
         });
     })
   });
+
+  describe('Post', function () {
+    it("should post promise result 1", function (done) {
+      promiseRouter.postAsync('/test', (req, locals) => {
+        return Promise.resolve({a: 1});
+      });
+
+      request(app)
+        .post('/test')
+        .expect({result: {a: 1}, ok: true})
+        .end(done);
+    });
+
+    it("should response with error stacktrace", function (done) {
+      promiseRouter.postAsync('/error', (req, locals) => {
+        return Promise.reject(new Error());
+      });
+
+      request(app)
+        .post('/error')
+        .expect(200)
+        .end(function (error, res) {
+          expect(res.body).to.have.property('msg');
+          expect(res.body).to.have.property('ok', false);
+          expect(res.body).to.have.property('code');
+          done();
+        });
+    })
+  });
 });
